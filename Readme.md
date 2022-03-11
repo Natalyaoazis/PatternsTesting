3c4c7fd8d3ae73b87edabf61246c5f89b074d5bd
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2022-03-11 13:35:50 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+
+# Patterns Testing
+[![Build status](https://ci.appveyor.com/api/projects/status/yag8kqerwc0reaqi?svg=true)](https://ci.appveyor.com/project/Natalyaoazis/patternstesting)
---
+++ src/test/java/ru/netology/UserGenerator.java
@@ -0,0 +1,80 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class UserGenerator {
+
+    @Value
+    public static class AuthUser {
+        String login;
+        String password;
+        String status;
+    }
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private UserGenerator() {
+    };
+    private static void sendRequest(AuthUser user) {
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+    public static AuthUser getValidActiveUser() {
+        AuthUser activeUser = new AuthUser(
+          faker.name().username(),
+          faker.internet().password(),
+          "active");
+        sendRequest(activeUser);
+        return activeUser;
+    }
+
+    public static AuthUser getValidBlockedUser() {
+        AuthUser blockedUser = new AuthUser(
+                faker.name().username(),
+                faker.internet().password(),
+                "blocked");
+        sendRequest(blockedUser);
+        return blockedUser;
+    }
+
+    public static AuthUser getInvalidPassword() {
+        String login = faker.name().username().toLowerCase();
+        AuthUser invalidPassword = new AuthUser(
+                login,
+                "validPassword",
+                "active");
+        sendRequest(invalidPassword);
+        return new AuthUser(login, "invalidPassword", "active");
+    }
+
+    public static AuthUser getInvalidLogin() {
+        String password = faker.internet().password();
+        AuthUser invalidLogin = new AuthUser(
+                "login",
+                password,
+                "active");
+        sendRequest(invalidLogin);
+        return new AuthUser("invalidLogin", password, "active");
+    }
+}
